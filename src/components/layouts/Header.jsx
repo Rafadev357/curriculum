d945b321bf9be1a8e851a8ad91bd6f3fbd271eca
Rafa <xanas_@hotmail.com>
2025-08-28 19:51:37 +0200

    Creacion de rutas y headers

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <div className='content__header'>
+      <nav className='header__nav'>
+        <div>Aquí va el logo</div>
+        <div>
+          <div>Home</div>
+          <div>Sobre mí</div>
+          <div>Proyectos</div>
+          <div>Curriculum</div>
+        </div>
+      </nav>
+    </div>
   )
 }
--- src/routing/RouterPrincipal.jsx
+++ src/routing/RouterPrincipal.jsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { BrowserRouter, Routes } from 'react-router-dom'
+import { Header } from '../components/layouts/Header'
 
 export const RouterPrincipal = () => {
   return (
-    <div>RouterPrincipal</div>
+    <BrowserRouter>
+        <Header/>
+        <Routes></Routes>
+    </BrowserRouter>
   )
 }
