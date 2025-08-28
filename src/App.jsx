8e8173f34bc8ba2d141ea3465058341914d15b54
Rafa <xanas_@hotmail.com>
2025-08-28 18:29:51 +0200

    Estructura de la pages home

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
+import { Home } from './components/pages/Home'
 
 function App() {
 
   return (
-    <>
-      
-    </>
+    <div className='App' >
+      <Home/>
+    </div>
   )
 }
 
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -2,6 +2,26 @@ import React from 'react'
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <div className='content'>
+        <div className='content__box'>
+            <div className='content__home'>
+                <div className='home__text'>
+                    <h2>¡Bienvenido!</h2>
+                    <h1>
+                        Soy
+                        <span className='spn__name'>Rafael Morales</span>
+                    </h1>
+                    <span className='spn__dev'>Web Developer</span>
+                </div>
+                <div className='home__picture'></div>
+            </div>
+        </div>
+        <div className='content__box'>
+            <div className="content__about">
+                <div className='about__text'></div>
+                <div className='about__picture'></div>
+            </div>
+        </div>
+    </div>
   )
 }
