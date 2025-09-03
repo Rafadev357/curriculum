137f53f60d399e47b079707b75723cbce454fc95
Rafa <xanas_@hotmail.com>
2025-09-03 19:17:05 +0200

    Update App.css and Home.jsx

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -28,18 +28,14 @@ export const Home = () => {
           <div className="about__picture">Aqui va una imagen</div>
           <div className="about__text">
             <h2>Un poco de mí</h2>
+            <div className="text__animation"></div>
             <p>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
-              gravida sed massa in dictum. Curabitur porttitor hendrerit nibh.
-              Praesent dapibus ultricies varius. Donec pretium varius sem a
-              molestie. Donec euismod tincidunt tempor. Proin sit amet sapien
-              ultrices, luctus lacus ac, laoreet justo. Nam vulputate odio
-              dignissim, varius leo non, tincidunt lectus. Cras vulputate erat
-              nec erat porta efficitur. Pellentesque lobortis quam vel neque
-              hendrerit consectetur. Cras dui mi, dapibus quis nisi a, lacinia
-              rhoncus erat. Morbi in eros vitae libero pharetra tempor eget
-              malesuada ipsum. Duis at quam ut eros lobortis faucibus nec quis
-              dui. Aliquam nec nisi non neque aliquam dapibus.
+              Soy un desarrollador de aplicaciones web con 35 años y una sólida
+              base en el ecosistema JavaScript. Mi experiencia abarca desde el
+              frontend con React hasta el backend con Node.js y MongoDB. Me
+              encantan los desafíos, me considero una persona curiosa y tengo
+              muchas ganas de seguir creciendo en esta nueva etapa de mi vida profesional.
+              Me apasiona resolver problemas y crear soluciones web eficientes y funcionales.
             </p>
           </div>
         </div>
